5c38de1e5ad9299c290aa782efe218ad4cbb8aae
Johannes Martinez <mart4695@morris.umn.edu>
2020-08-27 09:32:58 -0500

    Validate user input

--- compiling/extract_and_compile.sh
+++ compiling/extract_and_compile.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
-## Extract NthPrime.tgz
-## Options: -xf -> extract all files from the archive
+# Get a number input from user
+userNumberInput=""
+
+numRegex='[0-9]$' ## Regex for "a digit"
+
+# Ask user for number until a correct input is given
+# Based on https://stackoverflow.com/a/60274328
+until [[ $userNumberInput =~ $numRegex ]]
+do
+    echo Please enter a number
+    read userNumberInput
+done
+
+
+# Extract NthPrime.tgz
+# Options: -xf -> extract all files from the archive
 tar -xf NthPrime.tgz
